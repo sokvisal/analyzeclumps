3118697e25a5dd5e2dab33c9a14367bcbcd2b658
Visal <visal@localhost.localdomain>
2020-07-25 10:27:09 -0400

    new sed function

---
+++ seds.py
@@ -0,0 +1,29 @@
+import os
+import glob
+from tqdm import tqdm
+import subprocess
+import shutil
+
+def fit(path, tile):
+    directories = [idnames for idnames in glob.glob('{}/a{}/_id-*'.format(path, tile)) ] #'../run/selectedgal/resolved_sfgs/at064/_id*' #'../run/at065/_id*'
+    print ('Starting queue')
+    wdir = os.getcwd()
+
+    for i in directories[212:213]:
+
+        if os.path.isdir(i+'/test_phot'):
+            for dir in glob.glob('../analyzeclumps/sedfiles/*.param'):
+                shutil.copyfile(dir, i+'/test_phot/{}'.format(os.path.basename(dir)))
+            if os.path.isdir(i+'/test_phot/OUTPUT'):  shutil.rmtree(i+'/test_phot/OUTPUT')
+            os.makedirs(i+'/test_phot/OUTPUT')
+
+        if os.path.isfile(i+'/test_phot/cosmos.cat'):
+            os.chdir(i+'/test_phot/')
+
+            cmd = ['/home/epfl/sok/.local/fastpp/bin/fast++', 'fast.param']
+            f = open('fast.log', 'w')
+            subprocess.call(cmd)
+
+            os.chdir(wdir)
+        else:
+            pass
