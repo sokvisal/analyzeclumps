d8f7684ca4cca2cfdbc8c19a706c08eecb53ee18
Visal <visal@localhost.localdomain>
2020-07-27 14:46:24 -0400

    added eazy-phot

--- seds.py
+++ seds.py
@@ -9,20 +9,29 @@ def fit(path, tile):
     print ('Starting queue')
     wdir = os.getcwd()
 
-    for i in directories[:]:
+    for i in directories[:1]:
 
         if os.path.isdir(i+'/test_phot'):
             for dir in glob.glob('../analyzeclumps/sedfiles/*.param'):
                 shutil.copyfile(dir, i+'/test_phot/{}'.format(os.path.basename(dir)))
+            if os.path.isdir(i+'/test_phot/templates'): pass
+            else: shutil.copytree('./sedfiles/templates', i+'/test_phot/templates')
             if os.path.isdir(i+'/test_phot/OUTPUT'):  shutil.rmtree(i+'/test_phot/OUTPUT')
             os.makedirs(i+'/test_phot/OUTPUT')
 
         if os.path.isfile(i+'/test_phot/cosmos.cat'):
             os.chdir(i+'/test_phot/')
 
-            cmd = ['/home/epfl/sok/.local/fastpp/bin/fast++', 'fast.param']
-            f = open('fast.log', 'w')
-            subprocess.call(cmd)
+            zphotparams = ['zphot_2800-u.param', 'zphot_u-v.param']
+            for param in zphotparams:
+                ezycmd = ['/mnt/drivea/run/eazy-photoz/src/eazy', param]
+
+                f = open('eazy.log', 'w')
+                subprocess.call(ezycmd, stdout=f)
+
+            # cmd = ['/home/epfl/sok/.local/fastpp/bin/fast++', 'fast.param']
+            # f = open('fast.log', 'w')
+            # subprocess.call(cmd)
 
             os.chdir(wdir)
         else:
