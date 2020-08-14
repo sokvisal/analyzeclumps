4754d1a6c3cc1efa468d70335c6540cd8bf0613f
Visal <visal@localhost.localdomain>
2020-08-14 13:23:58 -0400

    test script

--- clumps.py
+++ clumps.py
@@ -278,4 +278,4 @@ def createCat(decpath, tile):
     # ids_clumps_cat = Table([ids_clumps[:,i] for i in range(14)],\
     #                        names=('id', 'z', 'lm', 'lm_res', 'lsfr', 'mclump', 'fuclump', 'uclump', 'vclump', 'cc_sm', 'cc_sfr', 'cm_density', 'issfr', 'ossfr'),\
     #                                meta={'name': 'cosmos clump id'})
-    # ascii.write(ids_clumps_cat, '{}/clumps-catalog-old.dat'.format(dirname[:-5]), overwrite=True, format='commented_header')
+    ascii.write(ids_clumps_cat, '{}/clumps-catalog.dat'.format(dirname[:-5]), overwrite=True, format='commented_header')
