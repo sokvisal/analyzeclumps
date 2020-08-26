e48f7b03297707f1103603f1a20d86b74d55854a
Visal <visal@localhost.localdomain>
2020-08-26 16:16:42 -0400

    test script

--- old_normporfiles.py
+++ old_normporfiles.py
@@ -641,9 +641,9 @@ def coadd_profile(prop, phot_vars, zphot):
         npix = []
 
         if mass:
-            maxr = a*0.7
+            maxr = a*0.6
         else:
-            maxr = a*0.7 #int(a*2)
+            maxr = a*0.6 #int(a*2)
         # print data.shape, len(xi)
         for i in np.arange(1,maxr):
             # b = i*np.sqrt(1-e**2)
