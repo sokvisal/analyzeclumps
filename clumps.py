6610ca7f3c84797f303c969b91209449b6c05277
sokvisal <visal.lesokgmail.com>
2021-03-30 15:33:41 -0400

    added a diagnostic of the uv luminosity contribution of clump

--- clumps.py
+++ clumps.py
@@ -251,7 +251,7 @@ def retrieved_maps(directories, path):
                 fig, clumpids, clumpiness, ccs = old_normporfiles.caddnorm_plot(rgbimg, maps, normmaps, params, [zp, idnum], res, show=False, savedir=False) #'{}/plots/{}'.format(path, tile)
 
 
-                newcat_clumps.append([idnum, zp,  galmass_idl, mass, galsfr] + clumpids + ccs + diagnostics)
+                newcat_clumps.append([idnum, zp,  galmass_idl, mass, galsfr] + clumpids + ccs + clumpiness + diagnostics)
 
 #                 if abs(galmass_idl-mass)>0.3:
 #                 misc.tmpsedfits(d, binshape)
@@ -275,7 +275,7 @@ def createCat(decpath, tile):
     #                        names=('id', 'z', 'lm', 'lm_res', 'lsfr', 'mclump', 'fuclump', 'uclump', 'vclump',\
     #                               'mfrac', 'fufrac', 'ufrac', 'vfrac', 'cc_sm', 'cc_sfr', 'cm_density', 'issfr', 'ossfr'),\
     #                                meta={'name': 'cosmos clump id'})
-    ids_clumps_cat = Table([ids_clumps[:,i] for i in range(14)],\
-                           names=('id', 'z', 'lm', 'lm_res', 'lsfr', 'mclump', 'fuclump', 'uclump', 'vclump', 'cc_sm', 'cc_sfr', 'cm_density', 'issfr', 'ossfr'),\
+    ids_clumps_cat = Table([ids_clumps[:,i] for i in range(15)],\
+                           names=('id', 'z', 'lm', 'lm_res', 'lsfr', 'mclump', 'fuclump', 'uclump', 'vclump', 'cc_sm', 'cc_sfr', 'cc_uv', 'cm_density', 'issfr', 'ossfr'),\
                                    meta={'name': 'cosmos clump id'})
-    ascii.write(ids_clumps_cat, '{}/clumps-catalog-photutils.dat'.format(dirname[:-5]), overwrite=True, format='commented_header')
+    ascii.write(ids_clumps_cat, '{}/clumps-catalog-hubble.dat'.format(dirname[:-5]), overwrite=True, format='commented_header')
